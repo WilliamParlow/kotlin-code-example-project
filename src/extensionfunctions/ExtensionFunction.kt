9917b6af882ed99f85fa17e41664aeba1eb54669
William Parlow <william.nichelatti@catolicasc.org.br>
2019-04-25 11:16:57 -0300

    Elaborated more examples about extension functions

--- src/extensionfunctions/ExtensionFunction.kt
+++ src/extensionfunctions/ExtensionFunction.kt
@@ -1,8 +1,36 @@
 package extensionfunctions
 
-fun String.hasSapces() = find { it == ' ' } != null
-
 fun main() {
-    println("Eu tenho espaco".hasSapces())
-    println("Eunaotenhoespaco".hasSapces())
+    println("I have spaces".hasSpaces())
+    println("Idonothavespaces".hasSpaces())
+
+    val candy = Candy("Strawberry")
+    candy.printFlavor()
+
+    val redCandy = ColouredCandy("Red", "cherry")
+    redCandy.printFlavor()
+
+    println(redCandy.isRed())
+    println(redCandy.isFlavorSizeToLong())
+    println(candy.isFlavorSizeToLong())
 }
+
+open class Candy(val flavor: String) {
+
+    open fun printFlavor() {
+        println("The candy flavor is $flavor")
+    }
+}
+
+class ColouredCandy(val color: String, flavor: String) : Candy(flavor) {
+
+    override fun printFlavor() {
+        println("The candy flavor is $flavor and it color is $color")
+    }
+}
+
+fun Candy.isFlavorSizeToLong() = flavor.length > 6
+
+fun ColouredCandy.isRed() = color == "Red"
+
+fun String.hasSpaces() = find { it == ' ' } != null
