c45ea4bfec42cc909639086378c9ed5fa7ea9057
William Parlow <william.nichelatti@catolicasc.org.br>
2019-04-29 14:31:01 -0300

    Out and In Example

---
+++ src/outandin/OutAndIn.kt
@@ -0,0 +1,51 @@
+package outandin
+
+fun main() {
+    outAndInExample()
+}
+
+open class WaterSupply(var needsProcessed: Boolean)
+
+class TapWater : WaterSupply(true) {
+    fun addChemicalCleaners() {
+        needsProcessed = false
+    }
+}
+
+class FishStoreWater : WaterSupply(false)
+
+class LakeWater : WaterSupply(true) {
+    fun filter() {
+        needsProcessed = false
+    }
+}
+
+class Aquarium<out T: WaterSupply>(val waterSupply: T) {
+
+    fun addWater(cleaner: Cleaner<T>) {
+        check(!waterSupply.needsProcessed) {
+            cleaner.clean(waterSupply)
+        }
+
+        println("adding water from $waterSupply")
+    }
+
+}
+
+interface Cleaner<in T: WaterSupply> {
+    fun clean(waterSupply: T)
+}
+
+class TapWaterCleaner: Cleaner<TapWater> {
+    override fun clean(waterSupply: TapWater) {
+        waterSupply.addChemicalCleaners()
+    }
+}
+
+fun addItemTo(aquarium: Aquarium<WaterSupply>) = println("item added")
+
+fun outAndInExample() {
+    val cleaner = TapWaterCleaner()
+    val aquarium = Aquarium(TapWater())
+    aquarium.addWater(cleaner)
+}
